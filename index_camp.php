ab4b51ba3a3991ebf7164cfa596d9bfa1cfe9a4e
dar73 <darshankubal1@gmail.com>
2023-04-15 15:11:57 +0530

    changes update 2023/04/15 03:11pm

--- index_camp.php
+++ index_camp.php
@@ -20,6 +20,18 @@ $PROFILE_ID = GetXFromYID("select profileid from role2profile where roleid='" . 
 //$User_division = GetXFromYID("select division from users where id='$USER_ID' "); //Getting division
 $SPECILITY_ARR = array();
 
+$ACTIVITY_ARR = GetXArrFromYID('SELECT id as id,`activityname`  as name FROM `crm_naf_activitymaster` where deleted=0', '3');
+
+$division = GetXFromYID("select division from users where id='$USER_ID' ");
+$curr_dt = date('Y-m-d');
+// $financial_qry = "SELECT financial_year FROM financialyear where '".$curr_dt."' between from_date and to_date";
+// $financial_qry_res = sql_query($financial_qry);
+$curr_fyear = GetXFromYID("SELECT financial_year FROM financialyear where '" . $curr_dt . "' between from_date and to_date ");
+
+$SPECILITY_ARR = GetXArrFromYID("select specialityid as id,specialityname as name from speciality where  in_use=0 AND fyear='$curr_fyear' and division='$division' ", '3');
+
+$selectoption = '';
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -72,58 +84,59 @@ $SPECILITY_ARR = array();
 
 	<div id="appCapsule">
 
-
-		<div class="tab-content mt-1">
-
-
-
-			<div class="section mt-2">
-				<div class="card">
-					<div class="card-body">
+		<form action="save_nafQtr.php" method="post">
+			<input type="hidden" name="userid" id="userid" value="<?php echo $USER_ID; ?>">
+			<div class="tab-content mt-1">
 
 
-						<div class="wide-block pt-2 pb-2">
 
-							<div class="row">
+				<div class="section mt-2">
+					<div class="card">
+						<div class="card-body">
 
-								<div class="col-3">
-									<b>Request Date:</b>
-								</div>
 
-								<div class="col-9">
-									<div class="input-wrapper">
-										<input type="text" class="form-control" id="" placeholder="<?php echo "" . date("d/m/Y") . ""; ?>" disabled>
+							<div class="wide-block pt-2 pb-2">
+
+								<div class="row">
+
+									<div class="col-3">
+										<b>Request Date:</b>
 									</div>
 
-								</div>
-							</div>
+									<div class="col-9">
+										<div class="input-wrapper">
+											<input type="text" class="form-control" name="reqDate" id="reqDate" value="<?php echo TODAY; ?>" readonly>
+										</div>
 
-							<br>
-
-							<div class="row">
-
-								<div class="col-3">
-									<b>Quarter:<span style="color:#ff0000">*</span></b>
-								</div>
-
-								<div class="col-9">
-									<div class="input-wrapper">
-										<select class="form-control custom-select" name="Quarter[]" multiple id="Quarter">
-											<option value="1">January to March</option>
-											<option value="2">April to June</option>
-											<option value="3">July to September</option>
-											<option value="4">October to December</option>
-										</select>
 									</div>
 								</div>
 
-							</div>
+								<br>
+
+								<div class="row">
+
+									<div class="col-3">
+										<b>Quarter:<span style="color:#ff0000">*</span></b>
+									</div>
+
+									<div class="col-9">
+										<div class="input-wrapper">
+											<select class="form-control custom-select" name="Quarter" id="Quarter">
+												<option value="1">January to March</option>
+												<option value="2">April to June</option>
+												<option value="3">July to September</option>
+												<option value="4">October to December</option>
+											</select>
+										</div>
+									</div>
+
+								</div>
 
 
 
-							<br>
+								<br>
 
-							<!--<div class="row">
+								<!--<div class="row">
 							
 								<div class="col-3">
 									<b>Name of the Activity:<span style="color:#ff0000">*</span></b>
@@ -139,39 +152,31 @@ $SPECILITY_ARR = array();
 							<br>-->
 
 
-							<div class="row">
+								<div class="row">
 
-								<div class="col-3">
-									<b>Activity Details (for Blanket Approval):</b>
-								</div>
-
-								<div class="col-9">
-									<div class="input-wrapper">
-
-										<b>Type of Activity (Please tick the appropriate box)</b>
-
-										<div class="custom-control custom-radio mb-1">
-											<input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
-											<label class="custom-control-label" for="customRadio1">Project Market Analysis</label>
-										</div>
-										<div class="custom-control custom-radio mb-1">
-											<input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
-											<label class="custom-control-label" for="customRadio2">Round Table Meeting</label>
-										</div>
-										<div class="custom-control custom-radio mb-1">
-											<input type="radio" id="customRadio3" name="customRadio" class="custom-control-input">
-											<label class="custom-control-label" for="customRadio3">Consultancy Engagement</label>
-										</div>
-
+									<div class="col-3">
+										<b>Activity Details (for Blanket Approval):</b>
 									</div>
+
+									<div class="col-9">
+										<div class="input-wrapper">
+
+											<b>Name of activity</b>
+
+											<div class="custom-control custom-radio mb-1">
+												<input type="text" class="form-control" name="activty_name" id="activty_name" required>
+											</div>
+
+
+										</div>
+									</div>
+
 								</div>
 
-							</div>
 
+								<br>
 
-							<br>
-
-							<div class="row">
+								<!-- <div class="row">
 
 								<div class="col-3">
 									<b>Please tick the appropriate box:</b>
@@ -204,60 +209,68 @@ $SPECILITY_ARR = array();
 									</div>
 								</div>
 
-							</div>
+							</div> -->
 
 
-							<br>
+								<br>
 
 
-							<div class="row">
+								<div class="row">
 
-								<div class="col-3">
-									<b>Proposed Count of Activities:<span style="color:#ff0000">*</span></b>
-								</div>
+									<div class="col-3">
+										<b>Proposed Count of Activities:<span style="color:#ff0000">*</span></b>
+									</div>
 
-								<div class="col-9">
-									<div class="input-wrapper">
-										<input type="number" class="form-control" id="" placeholder="" required="">
+									<div class="col-9">
+										<div class="input-wrapper">
+											<input type="number" name="proposed_count" class="form-control" id="proposed_count" placeholder="" required="">
+										</div>
 									</div>
 								</div>
-							</div>
 
 
-							<br>
+								<br>
 
 
-							<div class="row">
+								<div class="row">
 
-								<div class="col-3">
-									<b>Proposed Number of HCPS's:<span style="color:#ff0000">*</span></b>
-								</div>
+									<div class="col-3">
+										<b>Proposed Number of HCPS's:<span style="color:#ff0000">*</span></b>
+									</div>
 
-								<div class="col-9">
-									<div class="input-wrapper">
-										<input type="number" class="form-control" id="" placeholder="" required="">
+									<div class="col-9">
+										<div class="input-wrapper">
+											<input type="number" class="form-control" name="number_of_HCP" id="number_of_HCP" placeholder="" required="">
+										</div>
 									</div>
 								</div>
-							</div>
 
 
-							<br>
+								<br>
 
-							<div class="row">
+								<div class="row">
 
-								<div class="col-3">
-									<b>Proposed Nature of the Activity:<span style="color:#ff0000">*</span></b>
-								</div>
+									<div class="col-3">
+										<b>Proposed Nature of the Activity:<span style="color:#ff0000">*</span></b>
+									</div>
 
-								<div class="col-9">
-									<div class="input-wrapper">
-										<input type="text" class="form-control" id="" placeholder="" required="">
+									<div class="col-9">
+										<div class="input-wrapper">
+											<select class="form-control custom-select" id="Nature_of_activity" name="Nature_of_activity" required="" <?php echo $selectoption; ?>>
+												<?php
+												foreach ($ACTIVITY_ARR as $key => $value) {
+													//echo $value[$key]['iModID'];
+													$selected = '';
+													echo '<option value="' . $key . '" ' . $selected . ' >' . $value . '</option>';
+												}
+												?>
+											</select>
+										</div>
 									</div>
 								</div>
-							</div>
 
 
-							<!--<br>
+								<!--<br>
 							
 							<div class="row">
 							
@@ -282,162 +295,164 @@ $SPECILITY_ARR = array();
 
 
 
-							<br>
+								<br>
 
 
-							<div class="row">
+								<div class="row">
 
-								<div class="col-3">
-									<b>Estimated Number of Participants:<span style="color:#ff0000">*</span></b>
-								</div>
-
-								<div class="col-9">
-									<div class="input-wrapper">
-										<input type="number" class="form-control" id="" placeholder="" required="">
+									<div class="col-3">
+										<b>Estimated Number of Participants:<span style="color:#ff0000">*</span></b>
 									</div>
-								</div>
-							</div>
 
-
-							<br>
-							<div class="row">
-
-								<div class="col-3">
-									<b>Budgeted Amount in the Quarter:<span style="color:#ff0000">*</span></b>
-								</div>
-
-								<div class="col-9">
-									<div class="input-wrapper">
-										<input type="number" class="form-control" id="" placeholder="" required="">
-									</div>
-								</div>
-							</div>
-
-							<br>
-							<br>
-
-							<div class="row">
-
-								<div class="col-3">
-									<b>Targeted Speciality:<span style="color:#ff0000">*</span></b>
-								</div>
-
-
-								<div class="col-9">
-									<div class="input-wrapper">
-										<select class="form-control custom-select" name="targetedSpeciality[]" multiple id="targetedSpeciality">
-											<option value="1">Consultant Physician</option>
-											<option value="2">Dermatologist</option>
-											<option value="3">General Physician</option>
-											<option value="4">Gynaecologist</option>
-											<option value="5">Plastic Surgeon</option>
-										</select>
+									<div class="col-9">
+										<div class="input-wrapper">
+											<input type="number" class="form-control" id="number_of_participants" name="number_of_participants" placeholder="" required="">
+										</div>
 									</div>
 								</div>
 
-							</div>
 
-							<br>
-							<div class="row">
+								<br>
+								<div class="row">
 
-								<div class="col-3">
-									<b>The proposed objective/rationale/need for an Activity:<span style="color:#ff0000">*</span></b>
-								</div>
+									<div class="col-3">
+										<b>Budgeted Amount in the Quarter:<span style="color:#ff0000">*</span></b>
+									</div>
 
-								<div class="col-9">
-									<div class="input-wrapper">
-										<input type="text" class="form-control" id="" placeholder="" required="">
+									<div class="col-9">
+										<div class="input-wrapper">
+											<input type="number" class="form-control" id="budget_amt" name="budget_amt" placeholder="" required="">
+										</div>
 									</div>
 								</div>
-							</div>
+
+								<br>
+								<br>
+
+								<div class="row">
+
+									<div class="col-3">
+										<b>Targeted Speciality:<span style="color:#ff0000">*</span></b>
+									</div>
 
 
-							<br>
-							<div class="row">
+									<div class="col-9">
+										<div class="input-wrapper">
+											<select class="form-control custom-select" name="targetedSpeciality[]" multiple id="targetedSpeciality">
+												<?php
+												foreach ($SPECILITY_ARR as $key => $value) {
+													//echo $value[$key]['iModID'];
+													$selected = '';
+													echo '<option value="' . $key . '" ' . $selected . '>' . $value . '</option>';
+												}
+												?>
+											</select>
+										</div>
+									</div>
 
-								<div class="col-3">
-									<b>Rationale for selection of a particular Class of HCPs in comparison to other HCP’s:<span style="color:#ff0000">*</span></b>
 								</div>
 
-								<div class="col-9">
-									<div class="input-wrapper">
-										<input type="text" class="form-control" id="" placeholder="" required="">
+								<br>
+								<div class="row">
+
+									<div class="col-3">
+										<b>The proposed objective/rationale/need for an Activity:<span style="color:#ff0000">*</span></b>
+									</div>
+
+									<div class="col-9">
+										<div class="input-wrapper">
+											<input type="text" class="form-control" name="proposed_obj" id="proposed_obj" placeholder="" required="">
+										</div>
 									</div>
 								</div>
-							</div>
 
 
-							<br>
-							<div class="row">
+								<br>
+								<div class="row">
 
-								<div class="col-3">
-									<b>Will the event/activity benefit the society/Therapy/Patient in any manner? If yes, how?<span style="color:#ff0000">*</span></b>
-								</div>
+									<div class="col-3">
+										<b>Rationale for selection of a particular Class of HCPs in comparison to other HCP’s:<span style="color:#ff0000">*</span></b>
+									</div>
 
-								<div class="col-9">
-									<div class="input-wrapper">
-										<input type="text" class="form-control" id="" placeholder="" required="">
+									<div class="col-9">
+										<div class="input-wrapper">
+											<input type="text" class="form-control" id="rationale" name="rationale" placeholder="" required="">
+										</div>
 									</div>
 								</div>
-							</div>
 
 
+								<br>
+								<div class="row">
 
-							<br>
-							<div class="row">
+									<div class="col-3">
+										<b>Will the event/activity benefit the society/Therapy/Patient in any manner? If yes, how?<span style="color:#ff0000">*</span></b>
+									</div>
 
-								<div class="col-3">
-									<b>Who will lead the event from the Business Unit?<span style="color:#ff0000">*</span></b>
-								</div>
-
-								<div class="col-9">
-									<div class="input-wrapper">
-										<input type="text" class="form-control" id="" placeholder="" required="">
+									<div class="col-9">
+										<div class="input-wrapper">
+											<input type="text" class="form-control" id="activity_benefit" name="activity_benefit" placeholder="" required="">
+										</div>
 									</div>
 								</div>
-							</div>
 
 
-							<br>
-							<div class="row">
 
-								<div class="col-3">
-									<b>What is the medical equipment’s needed to be procured for the event/activity?<span style="color:#ff0000">*</span></b>
-								</div>
+								<br>
+								<div class="row">
 
-								<div class="col-9">
-									<div class="input-wrapper">
-										<input type="text" class="form-control" id="" placeholder="" required="">
+									<div class="col-3">
+										<b>Who will lead the event from the Business Unit?<span style="color:#ff0000">*</span></b>
+									</div>
+
+									<div class="col-9">
+										<div class="input-wrapper">
+											<input type="text" class="form-control" id="event_lead" name="event_lead" placeholder="" required="">
+										</div>
 									</div>
 								</div>
-							</div>
 
 
-							<!--<br>							
-							<div class="row">
-							
-								<div class="col-3">
-									<b>Deviation from the approved amount,if any:<span style="color:#ff0000">*</span></b>
-								</div>
-								
-								<div class="col-9">
-									<div class="input-wrapper">
-										<input type="number" class="form-control" id="" placeholder="" required="">
+								<br>
+								<div class="row">
+
+									<div class="col-3">
+										<b>What is the medical equipment’s needed to be procured for the event/activity?<span style="color:#ff0000">*</span></b>
+									</div>
+
+									<div class="col-9">
+										<div class="input-wrapper">
+											<input type="text" class="form-control" id="medical_equipment_needed" name="medical_equipment_needed" placeholder="" required="">
+										</div>
 									</div>
 								</div>
-							</div>-->
 
-							<br><br>
 
-							<!-- <div class="row">
+								<br>
+								<div class="row">
+
+									<div class="col-3">
+										<b>Deviation from the approved amount,if any:<span style="color:#ff0000">*</span></b>
+									</div>
+
+									<div class="col-9">
+										<div class="input-wrapper">
+											<input type="number" class="form-control" name="deviation_amount" id="deviation_amount" placeholder="" required="">
+										</div>
+									</div>
+								</div>
+
+								<br><br>
+
+								<!-- <div class="row">
 								<div class="col-3">
 									<b>Signature</b>
 								</div>
 							</div> -->
 
-							<br><br>
+								<br><br>
 
-							<!-- <div class="row">
+								<!-- <div class="row">
 								<div class="col-3">
 									Initiator’s Name: ___________________
 								</div>
@@ -467,57 +482,57 @@ $SPECILITY_ARR = array();
 								</div>
 							</div> -->
 
+							</div>
+
+
+
+
+
 						</div>
+					</div>
+				</div>
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+				<div class="section full mt-2">
+					<div class="wide-block pt-2 pb-2">
+
+						<div class="row">
+							<!-- <div class="col"><button type="button" class="exampleBox btn btn-primary rounded me-1">Save</button>
+							</div> -->
+							<div class="col">
+								<button type="submit" class="exampleBox btn btn-primary rounded me-1">Submit</button>
+							</div>
+							<!-- <div class="col">
+								<a href="#"><button type="button" class="exampleBox btn btn-primary rounded me-1">Cancel</button></a>
+							</div> -->
+						</div>
 
 					</div>
 				</div>
+
+
+
+
+
+
+
 			</div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-			<div class="section full mt-2">
-				<div class="wide-block pt-2 pb-2">
-
-					<div class="row">
-						<div class="col"><button type="button" class="exampleBox btn btn-primary rounded me-1">Save</button>
-						</div>
-						<div class="col">
-							<a href="naf_camp.php"><button type="button" class="exampleBox btn btn-primary rounded me-1">Submit</button></a>
-						</div>
-						<div class="col">
-							<a href="#"><button type="button" class="exampleBox btn btn-primary rounded me-1">Cancel</button></a>
-						</div>
-					</div>
-
-				</div>
-			</div>
-
-
-
-
-
-
-
-		</div>
-
-
-
+		</form>
 	</div>
 
 
@@ -532,11 +547,11 @@ $SPECILITY_ARR = array();
 	<!-- ///////////// Js Files ////////////////////  -->
 	<!-- Jquery -->
 	<script>
-		jQuery('#Quarter').multiselect({
-			columns: 1,
-			placeholder: 'Choose...',
-			search: true
-		});
+		// jQuery('#Quarter').multiselect({
+		// 	columns: 1,
+		// 	placeholder: 'Choose...',
+		// 	search: true
+		// });
 	</script>
 	<script src="assets/js/lib/jquery-3.4.1.min.js"></script>
 	<!-- Bootstrap-->
---
+++ save_nafQtr.php
@@ -0,0 +1,63 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include 'includes/common.php';
+//DFA($_POST);
+$pdo = connectToDatabase();
+
+$USER_ID = (isset($_POST['userid'])) ? $_POST['userid'] : '';
+$ROLE_ID = GetXFromYID("select roleid from user2role where userid='" . $USER_ID . "'");
+$PROFILE_ID = GetXFromYID("select profileid from role2profile where roleid='" . $ROLE_ID . "'");
+$User_division = GetXFromYID("select division from users where id='$USER_ID' "); //Getting division
+
+$DIVISION_ARR = GetXArrFromYID("select divisionid,name from division ", '3');
+$ACTIVITY_ARR = GetXArrFromYID('SELECT id as id,`activityname`  as name FROM `crm_naf_activitymaster` where deleted=0', '3');
+$empcode=GetXFromYID("select user_name from users where id='$USER_ID' and deleted=0 ");
+$initiater=GetXFromYID("SELECT CONCAT(first_name, ' ', last_name) AS result from users where id='$USER_ID' and deleted=0 ");
+
+
+$PENDING_WITH_ID = $STATUS = '';
+$crm_workflow = crm_workflow($USER_ID, 2, 1, 0);
+
+if (!empty($crm_workflow)) {
+    $PENDING_WITH_ID = $crm_workflow['pending_with_id'];
+    $STATUS = $crm_workflow['status'];
+}
+
+$reqDate = db_input($_POST['reqDate']);
+$Quarter = db_input($_POST['Quarter']);
+$activty_name = db_input($_POST['activty_name']);
+$proposed_count = db_input($_POST['proposed_count']);
+$number_of_HCP = db_input($_POST['number_of_HCP']);
+$Nature_of_activity = db_input($_POST['Nature_of_activity']);
+$number_of_participants = db_input($_POST['number_of_participants']);
+$budget_amt = db_input($_POST['budget_amt']);
+$proposed_obj = db_input($_POST['proposed_obj']);
+$rationale = db_input($_POST['rationale']);
+$activity_benefit = db_input($_POST['activity_benefit']);
+$event_lead = db_input($_POST['event_lead']);
+$medical_equipment_needed = db_input($_POST['medical_equipment_needed']);
+$deviation_amount = db_input($_POST['deviation_amount']);
+$targetedSpeciality_Arr = $_POST['targetedSpeciality'];
+
+
+$CrmID = NextID('id', 'crm_naf_main');
+$EVENT_ID = 'Q' . 'CAM' . substr($DIVISION_ARR[$User_division], 0, 3) . str_pad($CrmID, 4, '0', STR_PAD_LEFT);//Generating event ID 
+$Name = GetXFromYID("select first_name from users where id='$USER_ID' ");
+$pdo->prepare("insert into crm_naf_main (id,initiator,userid,emp_code,date,post_comment,category_id,submitted_on,submitted,pendingwithid,authorise,approved_date,deleted,deleted_on,eventdate,level,naf_no,naf_activity_name,naf_city,naf_proposed_venue,naf_estimate_no_participents,naf_start_date,naf_end_date,naf_objective_rational,remarks,quarter,mode,proposed_activity_count,proposed_hcp_no,proposed_activity,proposed_objective,rationale_remark,lead_event,medical_equipments,deviation_amount) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")->execute(array($CrmID,$initiater,$USER_ID,$empcode,NOW,'',2,NOW,1,$PENDING_WITH_ID,$STATUS,NOW,0,NULL,$reqDate,1,$EVENT_ID,$activty_name,0,'',$number_of_participants,NOW,NOW,$rationale,'',$Quarter,0,$proposed_count,$number_of_HCP,$Nature_of_activity,$proposed_obj,'',$event_lead,$medical_equipment_needed,$deviation_amount));
+
+$stmt = $pdo->prepare("insert into crm_naf_speciality_details(pid,naf_request_id,speciality_id,deleted) values(?,?,?,?) ");
+//$stmt->bind_param("iiii",$pid,$naf_request_id,$speciality_id,$deleted);
+foreach ($targetedSpeciality_Arr as $key => $value) {
+    $pid = NextID('pid', 'crm_naf_speciality_details');
+    $naf_request_id = $CrmID;
+    $speciality_id = $value;
+    $deleted = 0;
+    //$stmt->execute();
+    $stmt->execute(array($pid, $naf_request_id, $speciality_id, $deleted));
+}
+
+
+echo 'success';
+?>
